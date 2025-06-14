5a2eb0e0af39cd9f24da41278b1ba6a2552d116f
Jerome Tabiri <jtabiri@gmail.com>
2025-06-14 10:06:56 -0400

    disabled netbird ssh access in sg

--- deploy/sg.tf
+++ deploy/sg.tf
@@ -9,13 +9,13 @@ resource "aws_security_group" "netbird" {
     Purpose = "home-lab"
   }
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description = "SSH"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     description = "http"
